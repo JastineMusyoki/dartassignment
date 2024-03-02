b5c0a1269e53792f9882e07cccb3dec5e60ba8cc
JastineMusyoki <102594502+JastineMusyoki@users.noreply.github.com>
2024-03-02 16:53:56 +0300

    Assignment one

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num1=3;
+  double num2= 1.2;
+  double sum=num1+num2; // this is addition
+  List<String> names=["Fancy","jane","peter"];
+  Map<String, int> ages={"fancy":50,"eric":66,};
+  print(sum);
+  print(names);
+  print(ages);
+
+
+}
+  
